365f64e9966446eef8d2f9d30c51fa6e1724d20b
RafaelSSoares <rsilva.mj@gmail.com>
2022-07-20 20:42:37 -0300

    removing the first n characters

---
+++ removecharacter.py
@@ -0,0 +1,11 @@
+wordx = input('Enter: ')
+
+print(wordx[2:])
+
+# -------------------------------------
+
+# def rchars(word, n):
+#     x = word[n:]
+#     return x
+
+# print(rchars('Rafael', 2))
