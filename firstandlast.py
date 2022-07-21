d7df4bfaf8dfce92eea954602783e3902ccdc66a
RafaelSSoares <rsilva.mj@gmail.com>
2022-07-20 21:46:59 -0300

    first and last equals first and last

---
+++ firstandlast.py
@@ -0,0 +1,31 @@
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+if numbers_x[0] == numbers_x[-1]:
+    print('True.')
+else:
+    print('False.')
+
+if numbers_y[0] == numbers_y[-1]:
+    print('True.')
+else:
+    print('False.')
+
+# ------------------------------------------
+
+# def fls(nlist):
+#     print('Given list:', nlist)
+
+#     fnum = nlist[0]
+#     lnum = nlist[-1]
+
+#     if fnum == lnum:
+#         return True
+#     else:
+#         return False
+
+# numbers_x = [10, 20, 30, 40, 10]
+# print('Result is:', fls(numbers_x))
+
+# numbers_y = [75, 65, 35, 75, 30]
+# print('Result is:', fls(numbers_y))
